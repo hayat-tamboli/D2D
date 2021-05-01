40a8e4fced552cba5b1565587ff651924995c079
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-05-01 23:26:33 +0530

    home page made

--- lib/view/home/homePage.dart
+++ lib/view/home/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:tcard/tcard.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,11 +13,84 @@ class _HomePageState extends State<HomePage> {
   List<Widget> cards = List.generate(
     10,
     (index) => Container(
-      color: Colors.blue,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: Color(0xffd9dfe4),
+          width: 3,
+        ),
+        color: Color(0xffecf0f4),
+      ),
       child: Center(
-        child: Text(
-          '$index',
-          style: TextStyle(fontSize: 60, color: Colors.white),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1500048993953-d23a436266cf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80"),
+                      height: 180,
+                      width: 364,
+                    ),
+                  ),
+                ),
+                TranslucentCover(),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, top: 120),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Hayat Tamboli",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xfff9fcfd),
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        "Visual design, UI developer",
+                        style: TextStyle(
+                          color: Color(0xffecf0f4),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RatingBar.builder(
+                    initialRating: 3,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    itemSize: 30,
+                    itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: Color(0xff66D9AB),
+                    ),
+                    onRatingUpdate: (rating) {
+                      print(rating);
+                    },
+                  ),
+                ),
+                Text("3 out of 5"),
+              ],
+            ),
+          ],
         ),
       ),
     ),
@@ -65,7 +139,43 @@ class _HomePageState extends State<HomePage> {
               // tinder card here
               TCard(
                 cards: cards,
+                size: Size(380, 500),
+                //TODO: navigate to chat
+                onForward: (index, si) {
+                  print(si);
+                },
               ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TranslucentCover extends StatelessWidget {
+  const TranslucentCover({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        width: 364,
+        height: 180,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0x000f0f0f),
+              Color(0x0c0f0f0f),
+              Color(0x63101010),
+              Color(0xff101010)
             ],
           ),
         ),
