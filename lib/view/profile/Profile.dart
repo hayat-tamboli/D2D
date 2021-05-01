5d51c4d13a7eec1147d8a919a84201fa389f0fa3
Margi Bhatt <mbhatt2615@gmail.com>
2021-05-01 18:59:11 +0530

    Profile without settings done

--- lib/view/profile/Profile.dart
+++ lib/view/profile/Profile.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:random/widgets/projectCard.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  bool _workAvailable = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,17 +24,78 @@ class Profile extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: ProjectCard(
-            name: 'Hayat-Tamboli',
-            project: 'D2D',
-            contributions: 4,
-            issues: 11,
-            stars: 20,
-            forks: 3,
-            imageLink: 'https://avatars.githubusercontent.com/u/55529269?v=4',
-          ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(120.0, 10.0, 120.0, 10.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Image.network(
+                  'https://avatars.githubusercontent.com/u/55529269?v=4',
+                ),
+              ),
+            ),
+            Text(
+              'Hayat Tamboli',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+            ),
+            Text(
+              'UI/UX',
+              style: TextStyle(fontSize: 15.0, color: Colors.grey),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RatingBar.builder(
+                initialRating: 3,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                itemBuilder: (context, _) => Icon(
+                  Icons.star,
+                  color: Colors.tealAccent,
+                ),
+                onRatingUpdate: (rating) {
+                  print(rating);
+                },
+              ),
+            ),
+            SwitchListTile(
+              title: Text('Available for work'),
+              value: _workAvailable,
+              activeColor: Colors.blue,
+              onChanged: (bool value) {
+                setState(() {
+                  _workAvailable = value;
+                });
+              },
+            ),
+            SizedBox(width: 20.0, height: 20.0),
+            Container(
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        print("Open profile");
+                      });
+                    },
+                    child: ProjectCard(
+                      name: 'Hayat-Tamboli',
+                      project: 'D2D',
+                      contributions: 4,
+                      issues: 11,
+                      stars: 20,
+                      forks: 3,
+                      imageLink:
+                          'https://avatars.githubusercontent.com/u/55529269?v=4',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
