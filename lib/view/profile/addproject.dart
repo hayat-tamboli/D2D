ebda4cb8a2b95c4f2d12cf759f8de84c65ccc9f6
Manas1820 <manasgupta1820@gmail.com>
2021-05-02 02:16:06 +0530

    ⚡ Updated

--- lib/view/profile/addproject.dart
+++ lib/view/profile/addproject.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:random/Model/Project.dart';
+import 'package:random/Services/projectServices.dart';
+import 'package:random/widgets/button.dart';
+import 'package:random/widgets/inputBox.dart';
 
-class AddProject extends StatelessWidget {
+import '../../mainApp.dart';
+
+class AddProject extends StatefulWidget {
+  @override
+  _AddProjectState createState() => _AddProjectState();
+}
+
+class _AddProjectState extends State<AddProject> {
+  TextEditingController nameTextEditingController = new TextEditingController();
+  TextEditingController linkTextEditingController = new TextEditingController();
+  TextEditingController imgTextEditingController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +25,42 @@ class AddProject extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.all(16),
         child: Column(
-          children: [FlutterLogo(),FlutterLogo(),],
+          children: [
+            InputBox(
+              controller: nameTextEditingController,
+              textInputType: TextInputType.name,
+              hintText: 'Project Name',
+              maxLength: 36,
+            ),
+            InputBox(
+              controller: linkTextEditingController,
+              textInputType: TextInputType.name,
+              hintText: 'Project Link',
+              maxLength: 36,
+            ),
+            InputBox(
+              controller: imgTextEditingController,
+              textInputType: TextInputType.name,
+              hintText: 'Image Link',
+              maxLength: 36,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: PrimaryButton(
+                alt: false,
+                onTap: () async {
+                  Project p = Project.newProject(
+                      nameTextEditingController.toString(),
+                      linkTextEditingController.toString(),
+                      imgTextEditingController.toString());
+                  await addProject(p);
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MainApp()));
+                },
+                text: "Add Project",
+              ),
+            ),
+          ],
         ),
       ),
     );
