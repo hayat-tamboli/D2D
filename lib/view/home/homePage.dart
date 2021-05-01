3e91dab71c666e9cbb075259507ce2d76176ae00
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-05-02 05:12:13 +0530

    some commit message

--- lib/view/home/homePage.dart
+++ lib/view/home/homePage.dart
@@ -181,12 +181,171 @@ class _HomePageState extends State<HomePage> {
               ),
               // tinder card here
               TCard(
-                cards: cards,
+                cards: CardFunc(),
                 size: Size(380, 500),
-                //TODO: navigate to chat
                 onForward: (index, si) {
-                  print(si);
+                  if (si.direction == SwipDirection.Right) {
+                    //TODO: navigate to chat
+                  }
                 },
+              ),
+              //     TinderSwapCard(
+              //   swipeUp: true,
+              //   swipeDown: true,
+              //   orientation: AmassOrientation.BOTTOM,
+              //   totalNum: welcomeImages.length,
+              //   stackNum: 3,
+              //   swipeEdge: 4.0,
+              //   maxWidth: MediaQuery.of(context).size.width * 0.9,
+              //   maxHeight: MediaQuery.of(context).size.width * 0.9,
+              //   minWidth: MediaQuery.of(context).size.width * 0.8,
+              //   minHeight: MediaQuery.of(context).size.width * 0.8,
+              //   cardBuilder: (context, index) => Card(
+              //     child: welcomeImages[index],
+              //   ),
+              //   cardController: controller = CardController(),
+              //   swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
+              //     /// Get swiping card's alignment
+              //     if (align.x < 0) {
+              //       //Card is LEFT swiping
+              //     } else if (align.x > 0) {
+              //       Navigator.push(
+              //           context,
+              //           MaterialPageRoute(
+              //               builder: (context) => Conversation(
+              //                     chatRoomId:
+              //                         "CZfNA6b3R0U7wJnuHk41BpENrSK2_JaQRJkBEjbhpAwHOGzGIfqcEg7D3",
+              //                     usersNames: ["Hayat", "Hayat"],
+              //                     users: [
+              //                       "CZfNA6b3R0U7wJnuHk41BpENrSK2",
+              //                       "JaQRJkBEjbhpAwHOGzGIfqcEg7D3"
+              //                     ],
+              //                   )));
+              //       //Card is RIGHT swiping
+              //     }
+              //   },
+              //   swipeCompleteCallback:
+              //       (CardSwipeOrientation orientation, int index) {
+              //     /// Get orientation & index of swiped card!
+              //   },
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> CardFunc() {
+    return List.generate(
+      10,
+      (index) => Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: Color(0xffd9dfe4),
+            width: 3,
+          ),
+          color: Color(0xffecf0f4),
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "https://images.unsplash.com/photo-1500048993953-d23a436266cf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80"),
+                        height: 180,
+                        width: 364,
+                      ),
+                    ),
+                  ),
+                  TranslucentCover(),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16, top: 120),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hayat Tamboli",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color(0xfff9fcfd),
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          "Visual design, UI developer",
+                          style: TextStyle(
+                            color: Color(0xffecf0f4),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: RatingBar.builder(
+                      initialRating: 3,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      ignoreGestures: true,
+                      itemCount: 5,
+                      itemSize: 30,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Color(0xff66D9AB),
+                      ),
+                      onRatingUpdate: (rating) {
+                        print(rating);
+                      },
+                    ),
+                  ),
+                  Text("3 out of 5"),
+                ],
+              ),
+              Container(
+                height: 200,
+                child: GridView.builder(
+                    itemCount: 4,
+                    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 300,
+                      childAspectRatio: 3 / 2,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 8,
+                    ),
+                    itemBuilder: (BuildContext ctx, index) {
+                      return GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          clipBehavior: Clip.hardEdge,
+                          alignment: Alignment.center,
+                          child: Image(
+                            // height: 40,
+                            // width: 80,
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                                "https://cdn.dribbble.com/users/5732576/screenshots/14105722/media/084e7f222a56cec05d4aca936c889737.png"),
+                          ),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16)),
+                        ),
+                      );
+                    }),
               ),
             ],
           ),
