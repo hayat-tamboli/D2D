2d049061ada58a6df823e9e73b758c5d05db4b23
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-05-01 10:50:43 +0545

    After login structure

---
+++ lib/view/auth/afterLogin.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class afterLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text("You are a"),
+          ],
+        ),
+      ),
+    );
+  }
+}
